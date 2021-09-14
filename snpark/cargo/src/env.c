107335d19c5dcbd786d8327e48873e9b7f2c7b75
aaa <atimangel@naver.com>
2021-09-14 20:35:01 +0900

    환경 변수 초기 세팅 및 export 약간 구현 -strdup는 나중에 붙일 것 같아서 헤더에서 갖다 썼습니다. -export는 예외 처리 추가해야할 필요가 있습니다. -header에 추가하지 않아서 main에서 아직 쓸 수 없습니다. -env 함수는 업데이트 될 필요가 있습니다.

---
+++ snpark/cargo/src/env.c
@@ -0,0 +1,45 @@
+#include "minishell.h"
+#include <string.h>
+
+t_env	*export(char *str, t_env *head, int	flag)
+{
+	int	i;
+	t_env	*env;
+	t_env	*tmp;
+
+	i = 0;
+	while (str[i] != '=' || str[i] != '\0')
+		++i;
+	if (!str[i])  /* =이 없으면 멀쩡한 형식이 아님*/
+		return(NULL);
+	str[i] = '\0';
+	if (!str[0]) /* "=something" 이라 key값이 없는 상황임 */
+		retrun(NULL);
+	/* key 값이 중복되면? */
+		/* 그 key를 가진 구조체를 찾고, value 포인터를 free 한 후 새롭게 할당*/
+	/* key 값이 중복되지 않은 경우*/
+	env = (t_env *)malloc(1);
+	env->key = strdup(str);
+	env->value = strdup(str + ++i);
+	env->next = NULL;
+	if (flag == 1)
+		free(str);
+	if (head == NULL)
+		return (env);
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = env;
+	return (head);
+}
+
+t_env	*parent_env(char **old_env)
+{
+	t_env	*head;
+	int		i;
+
+	i = 0;
+	while (old_env[i] == NULL)
+		head = export(old_env[i++], head, 0);
+	return (head);
+}
