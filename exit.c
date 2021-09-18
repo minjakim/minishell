75280428d301ad0c19a40d9f3b593bb9e405966e
root <root@DESKTOP-0V4LBVQ.localdomain>
2021-09-18 15:00:23 +0900

    프롬프트 만듬 입력된 것을 그대로 출력함

--- exit.c
+++ exit.c
@@ -1,31 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/17 21:31:08 by snpark            #+#    #+#             */
-/*   Updated: 2021/09/18 14:28:14 by snpark           ###   ########.fr       */
+/*   Created: 2021/09/18 09:38:21 by snpark            #+#    #+#             */
+/*   Updated: 2021/09/18 10:49:26 by snpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-t_env	*env_list;
-
 int
-	main(int argc, char **argv, char **envp)
+	ft_exit(int error_number)
 {
-	char	buffer[256];
-	int		read_len;
-
-	while(1)
-	{
-		write(1, "> ", 2);
-		usleep(100000);
-		memset(buffer, 0, sizeof(char) * 256);
-		read_len = read(0, buffer, 256);
-		printf("%s", buffer);
-	}
+	exit(error_number);
 }
