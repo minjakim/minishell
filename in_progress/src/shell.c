ade9a221f9fc5525fbcf31a5c3370060154bc680
snpark <atimangel@google.com>
2021-09-24 17:05:21 +0900

    test: envp is not allocated

--- in_progress/src/shell.c
+++ in_progress/src/shell.c
@@ -1,14 +1,16 @@
 #include "../includes/minishell.h"
-#include <stdio.h>
+
+
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	char *line;
 
-	//parsing_env();
+	parsing_env();
 	printf("%s\n", envp[0]);
 	*envp[0]='a';
 	printf("%s\n", envp[0]);
+	free(*envp);
 //	set_signal();
 //	while(42)
 //	{
