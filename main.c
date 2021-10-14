ec14d9313d53f435241c5f56957f5049de9533ab
snpark <atimangel@google.com>
2021-10-14 19:12:05 +0900

    delete file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 21:31:08 by snpark            #+#    #+#             */
-/*   Updated: 2021/09/19 17:33:32 by snpark           ###   ########.fr       */
+/*   Updated: 2021/10/06 16:20:07 by snpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,5 @@ t_env	*env_list;
 int
 	main(int argc, char **argv, char **envp)
 {
-	char buffer[256];
-
-	while(1)
-	{
-		read(0, buffer, 256);
-		printf("%s", buffer);
-		memset(buffer, 0 ,256);
-	}
+	cd("MBC");
 }
