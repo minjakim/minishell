0f1349d9f551c345dcd0090ebb981a9b8a98be4d
Minjae Kim <minjakim@student.42seoul.kr>
2021-12-24 17:00:15 +0900

    훨훽인데 코드가 섞였다

--- src/execute/builtin_pwd.c
+++ src/execute/builtin_pwd.c
@@ -15,10 +15,11 @@
 int
 	builtin_pwd(const t_command *const cmd)
 {
-	const char *const	cwd = getcwd(NULL, 0);
+	char *const cwd = getcwd(NULL, 0);
 
 	if (cwd == NULL)
 		return (report_error(cmd->argv[0], NULL, errno));
 	printf("%s\n", cwd);
+	xfree(cwd);
 	return (g_status.exit = OK);
 }
